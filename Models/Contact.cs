4f32d0598f7c1313cb69565587ba595e18e32d19
Rami Aljanaby <rami.aljanaby@southeasttech.edu>
2024-10-17 10:47:15 -0500

    updated build with changes made by Chelsey

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,6 +1,33 @@
-﻿namespace ContactManagerT1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerT1.Models
 {
     public class Contact
     {
+        // Primary Key for the Contact table in the database
+        public int Id { get; set; }
+
+        // First name of the contact, marked as required (cannot be empty)
+        [Required]  // Ensures this field must have a value when creating or editing a contact
+        [Display(Name = "First Name")]  // Custom label for displaying the field in views
+        public string FirstName { get; set; }
+
+        // Last name of the contact, marked as required (cannot be empty)
+        [Required]  // Ensures this field must have a value
+        [Display(Name = "Last Name")]  // Custom label for displaying the field in views
+        public string LastName { get; set; }
+
+        // Phone number for the contact, marked as required and validated for phone format
+        [Required]  // Ensures this field must have a value
+        [Phone]  // Ensures the value must be a valid phone number format
+        public string Phone { get; set; }
+
+        // Email address for the contact, marked as required and validated for email format
+        [Required]  // Ensures this field must have a value
+        [EmailAddress]  // Ensures the value must be a valid email address format
+        public string Email { get; set; }
+
+        // Organization field is optional, and will store the organization the contact works for
+        public string Organization { get; set; }
     }
 }
