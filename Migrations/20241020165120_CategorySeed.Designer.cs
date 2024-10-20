47b69b0ff9ba77a07edc9d156c8c7edb941e6927
Rami Aljanaby <rami.aljanaby@southeasttech.edu>
2024-10-20 12:11:52 -0500

    app is ready for deployment, may need some visual tweaks

--- Migrations/20241020165120_CategorySeed.Designer.cs
+++ Migrations/20241020165120_CategorySeed.Designer.cs
@@ -4,6 +4,7 @@ using ContactManagerT1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContactManagerT1.Migrations
 {
     [DbContext(typeof(ContactContext))]
-    partial class ContactContextModelSnapshot : ModelSnapshot
+    [Migration("20241020165120_CategorySeed")]
+    partial class CategorySeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
